ed6116977dc973f00fd0ce22a1170a091e963bf8
neutronahemd <bazubairu@st.ug.edu.gh>
2023-12-07 23:20:56 +0000

    added missing bracket

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -5,4 +5,4 @@ from typing import Iterable, Sequence, List, Tuple
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Elemenet length"""
-    return [(i, len(i) for i in lst]
+    return [(i, len(i)) for i in lst]
