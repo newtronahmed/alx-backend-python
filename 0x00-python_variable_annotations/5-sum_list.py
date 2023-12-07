27f42df3574ae8cc25b165bdcc3d3f43cb0a63c9
neutronahemd <bazubairu@st.ug.edu.gh>
2023-12-07 23:01:46 +0000

    task 3, 4 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,6 @@
+"""Task 5 moduel"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ takes a list input_list of floats as argument and returns their sum"""
+    return float(sum(input_list))
