716aad30d72dd0c96b4f473ece5740fd02a02f5c
neutronahemd <bazubairu@st.ug.edu.gh>
2023-12-13 16:48:27 +0000

    task 0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Task 0 module"""
+import random
+import asyncio
+
+async def wait_random(max_delay=10):
+    rand = random.random() * max_delay
+    await asyncio.sleep(rand)
+    return rand 
---
+++ 0x01-python_async_function/README.md
@@ -0,0 +1,1 @@
+Alx python async functions
