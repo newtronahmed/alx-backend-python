96a00b788a7cd73eb41ab917340f379e049781cf
neutronahemd <bazubairu@st.ug.edu.gh>
2023-12-14 11:00:40 +0000

    type annotation for function parametr

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,7 +3,7 @@
 import random
 import asyncio
 
-async def wait_random(max_delay=10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
    """waits for a random delay between 0 and max_delay""" 
     rand = random.random() * max_delay
     await asyncio.sleep(rand)
