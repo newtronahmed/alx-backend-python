c641ad5bcc596182f47e2b2503f27ac73cb4eb2e
neutronahemd <bazubairu@st.ug.edu.gh>
2023-12-14 10:59:20 +0000

    used random.random() instead

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,6 +5,6 @@ import asyncio
 
 async def wait_random(max_delay=10) -> float:
    """waits for a random delay between 0 and max_delay""" 
-    rand = random.uniform(0, max_delay)
+    rand = random.random() * max_delay
     await asyncio.sleep(rand)
     return rand 
